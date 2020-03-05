d0bb83180c3e316bf3770fc337ff6d3451763d1c
erick <ericksotoe@gmail.com>
2020-03-04 20:47:41 -0800

    first commit need to work on unix

---
+++ POEScrape.py
@@ -0,0 +1,15 @@
+# from bs4 import BeautifulSoup as bs
+# import requests
+# from selenium import webdriver
+
+# class MyBot:
+#     def __inti__(self):
+#         self.driver = webdriver.Chrome()
+#         self.driver.get("https://www.instagram.com")
+
+# MyBot()
+
+import time
+from selenium import webdriver
+
+driver = webdriver.Chrome()
